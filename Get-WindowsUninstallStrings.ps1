bf28eeff24005898d44fe58df20595f8c53d9b3c
xxxmtixxx <clemons.paul.l@gmail.com>
2024-01-21 17:20:45 -0600

    Initial commit

---
+++ Get-WindowsUninstallStrings.ps1
@@ -0,0 +1,19 @@
+﻿$uninstallKeysPaths = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall', 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+$uninstallKeys = Get-ChildItem -Path $uninstallKeysPaths
+$applicationKeys = $uninstallKeys | Get-ItemProperty | Where-Object { $_.DisplayName -and $_.UninstallString }
+
+# Define the output file path
+$outputFilePath = "C:\Temp\UninstallStrings.txt"
+
+# Clear the output file if it already exists
+if (Test-Path $outputFilePath) {
+    Clear-Content $outputFilePath
+}
+
+foreach ($applicationKey in $applicationKeys) {
+    $applicationName = $applicationKey.DisplayName
+    $uninstallString = $applicationKey.UninstallString
+    Add-Content -Path $outputFilePath -Value "Application Name: $applicationName"
+    Add-Content -Path $outputFilePath -Value "Uninstall String: $uninstallString"
+    Add-Content -Path $outputFilePath -Value "-----------------------------"
+}
